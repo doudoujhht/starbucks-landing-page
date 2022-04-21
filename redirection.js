4082a60b4211c13b915d17c4bf4e98422d4fc031
doudoujhht <100162576+doudoujhht@users.noreply.github.com>
2022-04-21 10:43:38 -0400

    FONCTIONNELLLLLL

--- redirection.js
+++ redirection.js
@@ -5,15 +5,18 @@ red.forEach(element => {
     let j=element;
     element.addEventListener("click",(e) =>{
         localStorage.setItem("page",element)
-        let i= Math.floor(Math.random() * 2)
+        let i= Math.floor(Math.random() * 3)
 
         e.preventDefault();
         j=element;
         if(i==0){
             window.location = "loading-1/index.html"
         }
-        else{
+        else if(i==1){
             window.location = "loading-2/index.html"
+        }
+        else{
+            window.location = "loading-3/index.html"
         }
 
 
@@ -25,5 +28,5 @@ red.forEach(element => {
 function charger(){
     setTimeout(()=> {
         window.location = localStorage.getItem("page");
-    },((Math.random()*3)+2)*1000)
+    },((Math.random()*2)+2)*1000)
 }
