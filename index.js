cfdd12ff1076d35d4a1b909091b64d12d253542c
doudoujhht <100162576+doudoujhht@users.noreply.github.com>
2022-06-06 18:54:17 -0400

    modif perso

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const thumb1 = document.getElementById('thumb1');
 const thumb2 = document.getElementById('thumb2');
 const thumb3 = document.getElementById('thumb3');
 const circle = document.getElementById('circle');
+const nom = document.getElementById('nom');
+const button = document.getElementById('button');
+const logo = document.getElementById('logo');
 
 function slide(number, color){
     document.getElementById('main-image').src = `images/img${number}.png`;
     circle.style.backgroundColor = color;
+    button.style.backgroundColor = color;
+    nom.style.color = color;
+    logo.src =`images/logo${number}.png`
 }
