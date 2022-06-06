2bf2d973918d38f847165bf8f228a968a932aef2
doudoujhht <100162576+doudoujhht@users.noreply.github.com>
2022-06-06 19:01:45 -0400

    starbucks landing page version

--- horloge deuxieme version/index.js
+++
@@ -1,6 +0,0 @@
-
-// let heu = document.getElementById("heu");
-// let jour = new Date()
-// let hh = jour.getHours()*30;
-// let mm = jour.getHours()*6;
-// heu.style.transform = `rotateZ(${hh+(mm/12)}deg)`
--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-let navigation = document.querySelector('.navigation')
+const thumb1 = document.getElementById('thumb1');
+const thumb2 = document.getElementById('thumb2');
+const thumb3 = document.getElementById('thumb3');
+const circle = document.getElementById('circle');
+const nom = document.getElementById('nom');
+const button = document.getElementById('button');
+const logo = document.getElementById('logo');
 
-navigation.onclick = function(){
-    navigation.classList.toggle('active')
+function slide(number, color){
+    document.getElementById('main-image').src = `images/img${number}.png`;
+    circle.style.backgroundColor = color;
+    button.style.backgroundColor = color;
+    nom.style.color = color;
+    logo.src =`images/logo${number}.png`
 }
